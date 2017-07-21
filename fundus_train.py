d7c0c0ef24c6ed6ca156dbe382e0100c2eb65627
kimseongjung <plznw4me@naver.com>
2017-07-21 12:43:16 +0900

    add fundus_data

--- fundus_train.py
+++ fundus_train.py
@@ -13,9 +13,9 @@ import random
 def train():
     ##########################setting############################
     image_height, image_width, image_color_ch, n_classes, train_imgs_labs, test_imgs, test_labs = data.fundus_299x299()
-    model_saved_folder_path = utils.make_folder('./cnn_model/', 'optical/')
-    graph_saved_folder_path = utils.make_folder('./graph/', 'optical/')
-    log_saved_folder_path = utils.make_folder('./log/', 'optical/')
+    model_saved_folder_path = utils.make_folder('./cnn_model/', 'fundus/')
+    graph_saved_folder_path = utils.make_folder('./graph/', 'fundus/')
+    log_saved_folder_path = utils.make_folder('./log/', 'fundus/')
     f = open(log_saved_folder_path + 'log.txt', 'w+')
 
     x_ = tf.placeholder(dtype=tf.float32, shape=[None, image_height, image_width, image_color_ch], name='x_')
