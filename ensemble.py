054a2a2d7fd1abbcdb8e0ef59784863ba2ac531d
kimseongjung <plznw4me@naver.com>
2017-08-01 09:54:59 +0900

    reload

--- ensemble.py
+++ ensemble.py
@@ -9,6 +9,10 @@ cnn_model --fundus-- 0
                      1
                      2
                      3
+
+ensemble--result_saved_folder--target_folder
+    
+
 """
 
 
@@ -22,17 +26,15 @@ if __name__ == '__main__':
     if not os.path.isdir('./ensemble'):
         os.mkdir('./ensemble')
         print 'ensemble folder was created'
-    ensemble_save_path=os.path.join('./ensemble', args.path_dir.split('/')[-2]) #e.g ensemble_save_path= ./ensemble/cropped_original_fundus_300x300
+    ensemble_save_folder=os.path.join('./ensemble', args.path_dir.split('/')[-3]) #e.g ensemble_save_path= ./ensemble/cropped_original_fundus_300x300
     print 'folder name saved model',subfolder_names
     print  args.path_dir.split('/')[-3]
-    if not os.path.isdir(ensemble_save_path):
-        os.mkdir(os.path.join('./ensemble', args.path_dir.split('/')[-1]))
-        print 'folder created :  ',os.path.join('./cnn_models/ensemble', args.path_dir.split('/')[-1])
-        #e.g ./ensemble/cropped_original_fundus_300x300/
-    """
+    if not os.path.isdir(ensemble_save_folder):
+        os.mkdir(ensemble_save_folder)
+        print 'folder created :  ',ensemble_save_folder #e.g ./ensemble/cropped_original_fundus_300x300/
     sum_predict={}
     for i,subfolder_name in enumerate(subfolder_names):
-        target_model_folder=os.path.join(ensemble_save_path  , subfolder_name) #subfolder_name -->/1/ ,2,3,4,
+        target_model_folder=os.path.join(ensemble_save_folder  , subfolder_name) #subfolder_name -->/1/ ,2,3,4,
         # target_model_folder = ./ensemble/cropped_original_fundus_300x300/1/
         if not os.path.isdir(target_model_folder):
             os.mkdir(target_model_folder) #target_model_folder = ./ensemble/cropped_original_fundus_300x300/1/
@@ -49,4 +51,4 @@ if __name__ == '__main__':
         sum_predict[key]=sum_predict[key]/len(subfolder_names) #len(subfolder_names) => the number of model paths
 
     print 'the number of model' ,len(subfolder_files)
-    """
+
