a2adfd9de7943a7f23897aebdbb9831c75092720
kimseongjung <plznw4me@naver.com>
2017-08-11 12:13:45 +0900

    reload

--- fundus_train.py
+++ fundus_train.py
@@ -13,7 +13,7 @@ import argparse
 import os
 import time
 
-def train(max_iter ,learning_rate , check_point, nx=[10,10,10,5,5,5,35],, structure='inception_A', restored_model_folder_path=None , restored_path_folder_path=None):
+def train(max_iter ,learning_rate , check_point, nx=[10,10,10,5,5,5,35], structure='inception_A', restored_model_folder_path=None , restored_path_folder_path=None):
     ##########################setting############################
     image_height, image_width, image_color_ch, n_classes, \
     train_list_imgs_labs, test_list_imgs_labs, train_list_file_paths, test_list_file_paths,names = data.fundus_300x300(reload_folder_path=restored_path_folder_path)
@@ -103,7 +103,7 @@ def train(max_iter ,learning_rate , check_point, nx=[10,10,10,5,5,5,35],, struct
                     print 'model was saved!'
                     max_val = val_acc
 
-                print end_time- start_time
+                print 'time was consumed : ',end_time- start_time
                 start_time=time.time()
 
             # names = ['cataract', 'glaucoma', 'retina', 'retina_glaucoma','retina_cataract', 'cataract_glaucoma', 'normal']
