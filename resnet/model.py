6b2c6835e98f2febee649b23781aef152b506ecf
kimseongjung <plznw4me@naver.com>
2017-12-01 18:23:36 +0900

    fix some code :
    data/fundus_image

--- resnet/model.py
+++ resnet/model.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 import tensorflow as tf
 from cnn import convolution2d, batch_norm_layer, affine, max_pool, avg_pool , gap
 import cam
@@ -6,7 +7,8 @@ filters_per_blocks=[]
 n_blocks=[]
 
 class Resnet():
-    def __init__ (self , n_filters_per_box , n_blocks_per_box  , use_bottlenect , activation=tf.nn.relu , logit_type='gap'):
+    def __init__ (self , x_ , phase_train ,  n_filters_per_box , n_blocks_per_box  , use_bottlenect ,\
+                  activation=tf.nn.relu , logit_type='gap'):
         """
 
         :param n_filters_per_box: [32, 64, 64, 128 , 256 ]  , type = list
@@ -23,6 +25,8 @@ class Resnet():
         self.n_blocks_per_box = n_blocks_per_box
         self.n_boxes = len(n_filters_per_box)
         self.logit_type = logit_type
+        self.x_ = x_
+        self.phase_train = phase_train
         """
         n_blocks_per_box
         """
@@ -30,7 +34,7 @@ class Resnet():
         with tf.variable_scope('stem'):
             # conv filters out = 64
             layer = convolution2d('conv_0', x=x_, k=7, s=2)
-            layer = batch_norm_layer(layer, train_phase=, scope_bn='bn_0')
+            layer = batch_norm_layer(layer, phase_train= self.phase_train, scope_bn='bn_0')
             layer = self.activation(layer)
         for box_idx in range(self.n_boxes):
             with tf.variable_scope('box_{}'.foramt(box_idx)):
@@ -62,18 +66,18 @@ class Resnet():
         """ bottlenect layer """
         if self.use_bottlenect:
             with tf.variable_scope('bottlenect_{}'.format(block_n)):
-                layer = batch_norm_layer(layer , self.train_phase  , 'bn_0')
+                layer = batch_norm_layer(layer , self.phase_train  , 'bn_0')
                 layer = convolution2d('conv_0' , layer , out_ch = block_out_ch , k =1 , s =1 ) #fixed padding padding = "SAME"
-                layer = batch_norm_layer(layer, self.train_phase, 'bn_1')
+                layer = batch_norm_layer(layer, self.phase_train, 'bn_1')
                 layer = convolution2d('conv_1', layer, out_ch=block_out_ch, k=3,
                                       s=block_stride)  # fixed padding padding = "SAME"
-                layer = batch_norm_layer(layer, self.train_phase, 'bn_2')
+                layer = batch_norm_layer(layer, self.phase_train, 'bn_2')
                 layer = convolution2d('conv_2', layer, out_ch=block_out_ch, k=1, s=1)  # fixed padding padding = "SAME"
 
         else: #""" redisual layer """
             with tf.variable_scope('residual_{}.'.format(block_n)):
                 layer = convolution2d('conv_0' , layer , block_out_ch , k=3 , s=block_stride) # in here , if not block_stride = 1 , decrease image size
-                layer = batch_norm_layer(layer , self.train_phase,'bn_0' )
+                layer = batch_norm_layer(layer , self.phase_train,'bn_0' )
                 layer = convolution2d('conv_1', layer, block_out_ch, k=3, s=1)
 
         if not block_stride ==1: # image size 가 줄어들면 shortcut layer 의 이미지도 줄여야 한다
@@ -86,12 +90,17 @@ class Resnet():
             logit=gap('gap' , x , out_ch = self.n_classes)
         elif self.logit_type == 'fc':
 
-            layer=tf.cond(phase_train , lambda: tf.nn.dropout(x , keep_prob=0.5) , lambda: layer)
-            logit=affine('fc' , layer ,out_ch=self.n_classes , keep_prob=  )
+            logit=tf.cond(phase_train , lambda: affine('fc' , x ,out_ch=self.n_classes , keep_prob= 0.5 ) ,\
+                          lambda: affine('fc' , x ,out_ch=self.n_classes , keep_prob=1.0  ))
         else :
             print 'Not Implemneted , Sorry '
-
+        logit=tf.identity(logit , 'logit')
         return logit
 
 if __name__ =='__main__':
-    Resnet()
+    phase_train= True
+    x_ = tf.placeholder(dtype = tf.float32 , shape = [None , 32, 32 ,3 ] , name = 'x_')
+    n_filters_per_box = [16,16,32,32]
+    n_blocks_per_box = [5,5,5,5]
+    use_bottlenect = True
+    Resnet(x_ , phase_train , n_filters_per_box , n_blocks_per_box , use_bottlenect , activation=tf.nn.relu , logit_type='gap')
