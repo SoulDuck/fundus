4dac0fcd54dc66be62cbf92faa0f270ed65bd92d
kimseongjung <plznw4me@naver.com>
2017-12-02 12:41:58 +0900

    add tensoroard , model

--- fundus_resnet.py
+++ fundus_resnet.py
@@ -14,7 +14,7 @@ import os
 # tensorboard
 # model save
 # run training using global step
-train_imgs, train_labs, train_filenames, test_imgs, test_labs, test_filenames=data.type2('./fundus_300_debug')
+train_imgs, train_labs, train_filenames, test_imgs, test_labs, test_filenames=data.type2('./fundus_300')
 train_imgs=train_imgs/255.
 test_imgs = test_imgs/255.
 n_classes = 2
