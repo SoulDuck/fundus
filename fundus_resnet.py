567c4523b5c6549ee59c34c0383e04b54f8ea502
kimseongjung <plznw4me@naver.com>
2017-12-02 13:13:36 +0900

    add tensoroard , model

--- fundus_resnet.py
+++ fundus_resnet.py
@@ -26,7 +26,7 @@ aug_x_=aug.aug_tensor_images(x_ , phase_train ,  img_size_cropped=224 )
 n_filters_per_box = [8, 16, 32, 64]
 n_blocks_per_box = [2, 2, 2, 2]
 stride_per_box = [2, 2, 2, 2]
-use_bottlenect =  True
+use_bottlenect =  False
 
 model = resnet.Resnet(aug_x_, phase_train, n_filters_per_box, n_blocks_per_box, stride_per_box, \
                        use_bottlenect, n_classes=n_classes, activation=tf.nn.relu, logit_type='gap')
