b3db79276c35387413d0adbfa0867d28969eebaa
kimseongjung <plznw4me@naver.com>
2017-12-22 02:54:45 +0900

    add eval and ensemble

--- fundus_processing.py
+++ fundus_processing.py
@@ -155,6 +155,9 @@ def sparse_crop(image , crop_height , crop_width ,lr_flip =False, ud_flip=False 
     if lr_flip and ud_flip:
         lr_ud_flip_cropped_images = np.flip(ud_flip_cropped_images , axis=2)
         cropped_images = np.vstack((cropped_images , lr_ud_flip_cropped_images))
+
+    if lr_flip != False and ud_flip !=False:
+        cropped_images= ori_cropped_images
     utils.plot_images(cropped_images)
     return cropped_images
 
