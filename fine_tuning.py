c4e2eb594102257b32b2026e047cc61303d52e5c
kimseongjung <plznw4me@naver.com>
2018-01-07 10:15:27 +0900

    -fine-tuning -vgg lr iters  , lr_valeus

--- fine_tuning.py
+++ fine_tuning.py
@@ -13,7 +13,7 @@ parser=argparse.ArgumentParser()
 parser.add_argument('--ckpt_dir' , type=str  ) #default='finetuning_vgg_16'
 parser.add_argument('--batch_size' , type=int , default=40)
 parser.add_argument('--lr_iters' ,nargs='+', type=int, default=[5000 ,15000 , 40000 , 80000] )
-parser.add_argument('--lr_values',nargs='+', type=float, default=[1 , 0.7 , 0.4 , 0.01])
+parser.add_argument('--lr_values',nargs='+', type=float, default=[0.0001 , 0.00007 , 0.00004 , 0.00001])
 args=parser.parse_args()
 
 
