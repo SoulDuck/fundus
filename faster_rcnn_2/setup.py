8190e0c855e2ea710fe0743c551bb09dc88e4925
kimseongjung <plznw4me@naver.com>
2018-01-26 15:22:14 +0900

    ₩₩

--- faster_rcnn_2/setup.py
+++ faster_rcnn_2/setup.py
@@ -5,8 +5,8 @@ import sys
 sys.path.insert(0, '../')
 
 print
-setup(ext_modules = cythonize("./faster_rcnn_2/bbox_overlaps.pyx"),include_dirs=[np.get_include()])
-setup(ext_modules = cythonize("./faster_rcnn_2/bbox_transform.pyx"),include_dirs=[np.get_include()])
-setup(ext_modules = cythonize("./faster_rcnn_2/cpu_nms.pyx"),include_dirs=[np.get_include()])
+setup(ext_modules = cythonize("bbox_overlaps.pyx"),include_dirs=[np.get_include()])
+setup(ext_modules = cythonize("bbox_transform.pyx"),include_dirs=[np.get_include()])
+setup(ext_modules = cythonize("/cpu_nms.pyx"),include_dirs=[np.get_include()])
 
 
