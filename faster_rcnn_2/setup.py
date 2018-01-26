b13660aef59a12595366167bde92f49cf332828b
kimseongjung <plznw4me@naver.com>
2018-01-26 13:20:35 +0900

    add faster_rcnn

--- faster_rcnn_2/setup.py
+++ faster_rcnn_2/setup.py
@@ -1,6 +1,9 @@
 import numpy as np
 from distutils.core import setup
 from Cython.Build import cythonize
+import sys
+sys.path.insert(0, '../')
+
 setup(ext_modules = cythonize("./bbox_overlaps.pyx"),include_dirs=[np.get_include()])
 setup(ext_modules = cythonize("./bbox_transform.pyx"),include_dirs=[np.get_include()])
 setup(ext_modules = cythonize("./cpu_nms.pyx"),include_dirs=[np.get_include()])
