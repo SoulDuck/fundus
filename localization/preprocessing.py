0f116a43e63009880e926112296e53f743e2b5c6
kimseongjung <plznw4me@naver.com>
2018-01-29 22:28:19 +0900

    ₩₩

--- localization/preprocessing.py
+++ localization/preprocessing.py
@@ -1,5 +1,6 @@
 #-*- coding:utf-8 -*-
 import numpy as np
+import utils
 from fundus_processing import overlaps , get_width_height
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -153,13 +154,13 @@ class preprocessing(object):
 
 
                     for i,bg_coord in enumerate(cropped_coords):
+                        utils.show_progress(i, len(cropped_coords))
                         bg_coord = map(int, bg_coord)
                         bg_x1, bg_y1, bg_x2, bg_y2 = bg_coord
                         area = overlaps(bg_coord , fg_coord)
                         if np.max(cropped_images[i]) <= 30:
                             continue
                         if area == None or area <=int(fg_area*0.3):
-                            continue
                             bg_img=Image.fromarray(cropped_images[i])
                             plt.imsave(os.path.join(bg_dir,str(i))+'.png' ,bg_img )
                         elif area >=int(fg_area*0.8): # foreground Image에 90퍼 이상 겹치면 foregound Image로 분류한다
