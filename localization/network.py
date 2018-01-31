dd2dddc3df6762af3d71b8bb9de0637dd9d14961
kimseongjung <plznw4me@naver.com>
2018-01-31 10:42:17 +0900

    update utils saved_model

--- localization/network.py
+++ localization/network.py
@@ -160,7 +160,7 @@ class detection(network):
     def __init__(self , img_dir , crop_size):
         conv_filters = [3, 3, 3, 3, 3]
         conv_strides = [2, 2, 1, 1, 2, ]
-        conv_out_channels = [16, 16, 32, 32, 64]
+        conv_out_channels = [16, 32, 32, 64, 128]
         fc_out_channels = [1024, 1024]
         n_classes = 2
 
@@ -242,7 +242,7 @@ if __name__=='__main__':
 
     conv_filters=[3,3,3,3,3]
     conv_strides=[2,2,1,1,2,]
-    conv_out_channels=[64,64,128,128,256]
+    conv_out_channels=[16,32,32,64,128]
     fc_out_channels=[1024,1024]
 
     ##mnist version ###
