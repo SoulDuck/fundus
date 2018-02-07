00d6bc6a34bb2f6bc6e7d2a91a804d1b474767d8
kimseongjung <plznw4me@naver.com>
2018-02-07 11:49:32 +0900

    update utils saved_model

--- fine_tuning.py
+++ fine_tuning.py
@@ -367,9 +367,8 @@ if '__main__' == __name__ :
                 cost_list.append(batch_cost)
             val_acc = utils.get_acc(pred_list, test_labs)
             val_cost = np.sum(cost_list) / float(len(cost_list))
-            max_acc, min_loss = utils.save_model(model.sess, model.saver, max_acc, min_loss, val_acc, val_cost, best_acc_ckpt_dir,
-                                                 best_loss_ckpt_dir,
-                                                 step)
+            max_acc, min_loss = utils.save_model(model.sess, max_acc, min_loss, val_acc, val_cost, step, logs_path,
+                                                 model.last_saver, model.saver)
             utils.write_acc_loss(tb_writer, prefix='test', loss=val_cost, acc=val_acc, step=step)
             utils.write_acc_loss(tb_writer, prefix='train', loss=loss, acc=acc, step=step)
             lr_summary = tf.Summary(value=[tf.Summary.Value(tag='learning_rate', simple_value=float(lr))])
